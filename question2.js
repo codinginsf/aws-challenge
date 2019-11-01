b835ab7949566a47a5fd77513b651aa4732f495b
TheSiaWay <thesiaway@gmail.com>
2019-10-31 19:39:12 -0700

    refactor q2

--- question2.js
+++ question2.js
@@ -5,11 +5,12 @@ const groupNumByThree = (num) => {
         num = Math.floor(num / 1000);
     }
 
-    return thousands;
+    return thousands.reverse();
 };
 
-const convertByHundred = (num, place = 1) => {
-  const teens = {
+const convertByHundred = (n) => {
+  let str = '';
+  const key = {
     0: "zero",
     1: "one",
     2: "two",
@@ -30,18 +31,6 @@ const convertByHundred = (num, place = 1) => {
     17: "seventeen",
     18: "eighteen",
     19: "nineteen",
-  };
-  const adults = {
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
     20: "twenty",
     30: "thirty",
     40: "forty",
@@ -49,113 +38,49 @@ const convertByHundred = (num, place = 1) => {
     60: "sixty",
     70: "seventy",
     80: "eighty",
-    90: "ninety",
-  };
-  const elders = {
-    100: "hundred",
-    1000: "thousand",
-    1000000: "million"
-  };
-
-  const singleDigits = {
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9
+    90: "ninety"
   }
-
-  if (num < 20) {
-    return teens[num];
-  }
-
-  const arr = [];
-  while (num > 0) {
-    const current = num % 10;
-    num = Math.floor(num / 10);
-
-    switch (place) {
-      case 1:
-        arr.push(teens[current]);
-        break;
-      case 2:
-        const realNumber = (current * 10) + singleDigits[arr[0]];
-        if (realNumber < 20) {
-          arr.push(adults[realNumber]);
-          arr[0] = "";
-        } else {
-          arr.push(adults[current * 10]);
-        }
-        break;
-      case 3:
-        arr.push(`${teens[current]} hundred and `);
-        break;
-      case 4:
-        break;
-
-      default:
-        break;
+  if (n === 0) return key[0];
+  while (n > 0) {
+    if (n < 20) {
+      str += key[n];
+      return str;
     }
-
-    place += 1;
-  }
-
-  let ans = "";
-  for (let i = arr.length - 1; i >= 0; i--) {
-    const currentNumberInWord = arr[i];
-    if (currentNumberInWord == "zero") {
-      continue;
+    if (n < 100) {
+      const ten = Math.floor(n / 10) * 10;
+      const one = n % 10;
+      str += `${key[ten]}-${key[one]}`;
+      return str;
     }
-
-    if (i === 0 && currentNumberInWord) {
-      ans += `-${currentNumberInWord}`;
-    } else {
-      ans += currentNumberInWord;
+    const hundreds = Math.floor(n / 100);
+    n %= 100;
+    str += `${key[hundreds]} hundred`;
+    if (n > 0) {
+      str += ' and '
     }
   }
-
-  return ans;
+  return str;
 };
 
 const convertNumberToWords = (num) => {
-    const delta = num - Math.floor(num);
-    num = Math.floor(num);
-    const chunkedArr = groupNumByThree(num)
-    let decimalPlaces = chunkedArr.length;
-    let numberInStr = "";
-    while (chunkedArr.length) {
-        const current = chunkedArr.pop();
-        if (current > 0) {
-            numberInStr += convertByHundred(current);
+  const key = ['', 'thousand', 'million']
+  const cent = Math.round((num - Math.floor(num)) * 100);
+  num = Math.floor(num);
+  const chunkedArr = groupNumByThree(num)
+  let str = "";
+  let nonZero = chunkedArr.filter(el => el > 0).length;
 
-            switch (decimalPlaces) {
-                case 2:
-                    if (chunkedArr.every(val => val > 0)) {
-                        numberInStr += " thousand, ";
-                    } else {
-                        numberInStr += " thousand";
-                    }
-                    decimalPlaces--;
-                    break;
-                case 3:
-                    if (chunkedArr.every(val => val > 0)) {
-                        numberInStr += " million, ";
-                    } else {
-                        numberInStr += " million";
-                    }
-                    decimalPlaces--;
-                    break;
-                default:
-                    break;
-            }
-        };
+  chunkedArr.forEach((el, i) => {
+    if (el > 0) {
+      const val = chunkedArr.length - i - 1
+      str += `${convertByHundred(el)} ${key[val]}`
+      nonZero--;
     }
-
-    const capitalizedNumInStr = numberInStr[0].toUpperCase() + numberInStr.slice(1);
-    return delta ? capitalizedNumInStr + ` and ${ Math.round(delta * 100) } /100` : capitalizedNumInStr;
+    if (nonZero > 0) {
+      str += ', '
+    }
+  })
+  str = str.trim();
+  const capitalizedNumInStr = str[0].toUpperCase() + str.slice(1);
+  return cent ? capitalizedNumInStr + ` and ${cent} /100` : capitalizedNumInStr;
 }
